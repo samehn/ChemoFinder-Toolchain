e2a5be0a91f791d62e3279abd1b70c78fa18916e
karim-elbawab <karim.elbawab@gmail.com>
2017-03-14 16:40:20 +0200

    fixing medicines list issues

--- models/model.js
+++ models/model.js
@@ -42,26 +42,9 @@ model.prototype.mysql_real_escape_string = function(str) {
     console.log(str);
     str = "" + str;
     return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
-        switch (char) {
-            case "\0":
-                return "\\0";
-            case "\x08":
-                return "\\b";
-            case "\x09":
-                return "\\t";
-            case "\x1a":
-                return "\\z";
-            case "\n":
-                return "\\n";
-            case "\r":
-                return "\\r";
-            case "\"":
-            case "'":
-            case "\\":
-            case "%":
-                return "\\"+char; // prepends a backslash to backslash, percent,
-                                  // and double/single quotes
-        }
+        var m = ['\\0', '\\x08', '\\x09', '\\x1a', '\\n', '\\r', "'", '"', "\\", '\\\\', "%"];
+        var r = ['\\\\0', '\\\\b', '\\\\t', '\\\\z', '\\\\n', '\\\\r', "''", '""', '\\\\', '\\\\\\\\', '\\%'];
+        return r[m.indexOf(char)];
     });
 }
 
